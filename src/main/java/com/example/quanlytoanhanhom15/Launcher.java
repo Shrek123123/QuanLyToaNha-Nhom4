fc5bb4a796f0cce1129f7534dcb0d1832699d2e1
Shrek123123 <qudominh16@gmail.com>
2025-10-29 18:50:36 +0700

    tạo repo

---
+++ src/main/java/com/example/quanlytoanhanhom15/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.quanlytoanhanhom15;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
